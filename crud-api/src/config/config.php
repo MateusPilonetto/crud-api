538158d05e1520c006a4f089f28fc024a22fe124
Mateus Pilonetto <pilonettoantoniomateus@gmail.com>
2026-07-15 15:35:58 -0300

    chore: Update .gitignore to include full path for config.php

--- crud-api/src/config/config.php
+++ crud-api/src/config/config.php
@@ -3,7 +3,7 @@ const DB_HOST = 'db';
 const DB_NAME = 'store'; 
 const DB_USER = 'root';
 const DB_PASS = 'DB230517'; 
-
+ 
 $allowedOrigins = [
     'http://0.0.0.0:8080', 'http://localhost:8080', 'http://127.0.0.1:8080',
     'http://0.0.0.0:5500', 'http://localhost:5500', 'http://127.0.0.1:5500',
